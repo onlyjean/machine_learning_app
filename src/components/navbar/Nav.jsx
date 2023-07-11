477149c381cbbdc709a1885773e166eedeb4d348
Jean Isou <onlyjean8888@gmail.com>
2023-07-11 15:30:06 +0100

    added ref to algo section

--- src/components/navbar/Nav.jsx
+++ src/components/navbar/Nav.jsx
@@ -8,7 +8,7 @@ const Menu = () => (
   <>
   <p><a href='#home'>Home</a></p>
   <p><a href='#about'>About Us</a></p>
-  <p><a href='#machine'>Machine Learning</a></p>
+  <p><a href='#algo'>Machine Learning</a></p>
   <p><a href='#features'>Capabilities</a></p>
   <p><a href='#stock'>Stock Market</a></p>
   </>
