c9ee72a5e41cedced6d5ede2899331ae18c481b2
Jean Isou <onlyjean8888@gmail.com>
2023-08-01 21:00:43 +0100

    added stripe webhook

--- amplify/backend/function/StripePayment/src/app.js
+++ amplify/backend/function/StripePayment/src/app.js
@@ -14,6 +14,10 @@ const bodyParser = require('body-parser')
 const awsServerlessExpressMiddleware = require('aws-serverless-express/middleware')
 // stripe import
 const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
+const AWS = require('aws-sdk');
+const cognito = new AWS.CognitoIdentityServiceProvider();
+
+
 
 
 // declare a new express app
@@ -30,6 +34,9 @@ app.use(function(req, res, next) {
 
 
 app.post('/create-checkout-session', async (req, res) => {
+
+  const { username } = req.body;  // Extract username from the request body
+
   const session = await stripe.checkout.sessions.create({
     payment_method_types: ['card'],
     line_items: [{
@@ -39,12 +46,85 @@ app.post('/create-checkout-session', async (req, res) => {
     mode: 'subscription',
     success_url: 'http://localhost:3000/success?session_id={CHECKOUT_SESSION_ID}',
     cancel_url: 'http://localhost:3000/signedin',
+    client_reference_id: username,  // Set the client_reference_id to the username
+    metadata: {  // Add metadata field
+      username: username,  // Include username in the metadata
+    },
   });
   res.json({ id: session.id });
 });
 
 
+app.post('/webhook', bodyParser.raw({type: 'application/json'}), async (req, res) => {
+  const sig = req.headers['stripe-signature'];
 
+  let event;
+
+  try {
+    event = stripe.webhooks.constructEvent(req.body, sig, process.env.STRIPE_WEBHOOK_SECRET);
+  } catch (err) {
+    return res.status(400).send(`Webhook Error: ${err.message}`);
+  }
+
+    // Log the event type and data
+    console.log(`Received event with type ${event.type}`);
+    console.log(`Event data: ${JSON.stringify(event.data)}`);
+  
+
+  // Handle the checkout.session.completed event
+  if (event.type === 'checkout.session.completed') {
+    console.log("checkout.session.completed event received");
+      const session = event.data.object;
+
+    // Get the user's username from the client reference ID
+    const username = session.client_reference_id;
+
+    console.log(`Username: ${username}`);
+    console.log(`Customer ID: ${session.customer}`);
+
+    // Update the user's Stripe customer ID in Cognito
+    try {
+      await cognito.adminUpdateUserAttributes({
+        UserPoolId: 'eu-west-2_d852nM2qL',
+        Username: username,
+        UserAttributes: [
+          {
+            Name: 'custom:stripeCustomerId',
+            Value: session.customer,
+          },
+        ],
+      }).promise();
+      console.log(`Updated Stripe customer ID for user ${username}`);
+    } catch (error) {
+      console.error(`Error updating user attributes: ${error}`);
+    }
+  }
+    
+
+  // Handle the customer.subscription.deleted event
+  if (event.type === 'customer.subscription.deleted') {
+    const subscription = event.data.object;
+
+    // Get the user's username from the client reference ID
+    const username = subscription.metadata.username;
+
+    // Update the user's Stripe customer ID in Cognito to be empty
+    await cognito.adminUpdateUserAttributes({
+      UserPoolId: 'eu-west-2_d852nM2qL',
+      Username: username,
+      UserAttributes: [
+        {
+          Name: 'custom:stripeCustomerId',
+          Value: '',
+        },
+      ],
+    }).promise();
+
+    console.log(`Cleared Stripe customer ID for user ${username}`);
+  }
+
+  res.json({received: true});
+});
 
 
 /**********************
