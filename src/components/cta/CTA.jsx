36c46d4fa0e600311ea1830db1435e666e002630
Jean Isou <onlyjean8888@gmail.com>
2023-08-04 22:15:43 +0100

    cta

--- src/components/cta/CTA.jsx
+++ src/components/cta/CTA.jsx
@@ -2,15 +2,23 @@ import React from 'react'
 import './cta.css'
 
 const CTA = ({ isSubscribed }) => {  // isSubscribed is a prop that indicates whether the user is subscribed or not
+  // const handleLSTMClick = () => {
+  //   if (isSubscribed) {
+  //     // If the user is subscribed, redirect them to the Streamlit app
+  //     window.open("https://lstm-streamlit.onrender.com", '_blank');
+  //       } else {
+  //     // If the user is not subscribed, show an error message
+  //     alert("You must be a subscribed customer to access this feature.");
+  //   }
+  // }
+
   const handleLSTMClick = () => {
-    if (isSubscribed) {
-      // If the user is subscribed, redirect them to the Streamlit app
+        // Redirect the user to the Streamlit app without checking the subscription status
       window.open("https://lstm-streamlit.onrender.com", '_blank');
-        } else {
-      // If the user is not subscribed, show an error message
-      alert("You must be a subscribed customer to access this feature.");
-    }
+    
   }
+
+
 
     const handleLRClick = () => {
     // Redirect the user to the Streamlit app without checking the subscription status
