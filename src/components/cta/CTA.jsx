ca5df56fd214b204169d103feca66537f73bd2e0
Jean Isou <onlyjean8888@gmail.com>
2023-08-04 15:38:10 +0100

    open links in new tabs

--- src/components/cta/CTA.jsx
+++ src/components/cta/CTA.jsx
@@ -2,11 +2,11 @@ import React from 'react'
 import './cta.css'
 
 const CTA = ({ isSubscribed }) => {  // isSubscribed is a prop that indicates whether the user is subscribed or not
-  const handleClick = () => {
+  const handleLSTMClick = () => {
     if (isSubscribed) {
       // If the user is subscribed, redirect them to the Streamlit app
-      window.location.href = "http://localhost:8501/";
-    } else {
+      window.open("https://lstm-streamlit.onrender.com", '_blank');
+        } else {
       // If the user is not subscribed, show an error message
       alert("You must be a subscribed customer to access this feature.");
     }
@@ -14,7 +14,12 @@ const CTA = ({ isSubscribed }) => {  // isSubscribed is a prop that indicates wh
 
     const handleLRClick = () => {
     // Redirect the user to the Streamlit app without checking the subscription status
-    window.location.href = "http://localhost:8501/";
+    window.open("https://linear-regression-streamlit.onrender.com", '_blank');
+    }
+
+  const handleTradeClick = () => {
+    // Redirect the user to the Streamlit app without checking the subscription status
+    window.open("https://freetrade.io", '_blank');  
   }
   
 
@@ -25,8 +30,10 @@ const CTA = ({ isSubscribed }) => {  // isSubscribed is a prop that indicates wh
             <h3>Play with and train our models</h3>
         </div>
         <div className='ml__cta-button'>
-           <button type='button' onClick={handleClick}>Linear Regression</button> 
-            <button type='button' onClick={handleLRClick}>LR</button> 
+           <button type='button' onClick={handleLSTMClick}>LSTM</button> 
+            <button type='button' onClick={handleLRClick}>Linear Regression</button> 
+            <button type='button' onClick={handleTradeClick}>Click to Trade</button> 
+
         </div>
     </div>
   )
