0f15576fe3b03853c05d54a9d8eb2f592d3ec865
Jean Isou <onlyjean8888@gmail.com>
2023-08-18 20:13:07 +0100

    stripe webhook is working properly

--- amplify/backend/function/StripePayment/src/app.js
+++ amplify/backend/function/StripePayment/src/app.js
@@ -1,193 +1,193 @@
-/*
-Copyright 2017 - 2017 Amazon.com, Inc. or its affiliates. All Rights Reserved.
-Licensed under the Apache License, Version 2.0 (the "License"). You may not use this file except in compliance with the License. A copy of the License is located at
-    http://aws.amazon.com/apache2.0/
-or in the "license" file accompanying this file. This file is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and limitations under the License.
-*/
-
-
-
-
-const express = require('express')
-const bodyParser = require('body-parser')
-const awsServerlessExpressMiddleware = require('aws-serverless-express/middleware')
-// stripe import
+const express = require('express');
+const bodyParser = require('body-parser');
+const awsServerlessExpressMiddleware = require('aws-serverless-express/middleware');
 const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
 const AWS = require('aws-sdk');
 const cognito = new AWS.CognitoIdentityServiceProvider();
+const fetch = require('node-fetch');
+const aws4 = require('aws4');
 
+const app = express();
+// app.use(bodyParser.json());
+app.use(awsServerlessExpressMiddleware.eventContext());
 
-
-
-// declare a new express app
-const app = express()
-app.use(bodyParser.json())
-app.use(awsServerlessExpressMiddleware.eventContext())
-
-// Enable CORS for all methods
 app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*")
-  res.header("Access-Control-Allow-Headers", "*")
-  next()
+    console.log("Received request:", req.method, req.url);
+    console.log("Request headers:", req.headers);
+    console.log("Request body:", req.body);
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "*");
+    next();
 });
 
+const makeGraphQLRequest = async (query, variables) => {
+    console.log("Making GraphQL request with query:", query);
+    console.log("Variables:", variables);
 
-app.post('/create-checkout-session', async (req, res) => {
+    const requestBody = {
+        query: query,
+        variables: variables
+    };
 
-  const { username } = req.body;  // Extract username from the request body
+    const signedRequest = aws4.sign({
+        method: 'POST',
+        url: 'https://y4phlwteercpflrdx6pawip3dm.appsync-api.eu-west-2.amazonaws.com/graphql',
+        headers: {
+            'Content-Type': 'application/json',
+            'x-api-key': 'da2-q5rjn4gqwnfyza3h4pccmvcipm'
+        },
+        body: JSON.stringify(requestBody),
+        service: 'appsync',
+        region: 'eu-west-2'
+    });
 
-  const session = await stripe.checkout.sessions.create({
-    payment_method_types: ['card'],
-    line_items: [{
-      price: 'price_1Nb7sTGGfDL03j8nedwYY4zH',  // Using the price ID directly
-      quantity: 1,
-    }],
-    mode: 'subscription',
-    success_url: 'https://main.dsxr40yvbyhag.amplifyapp.com/success?session_id={CHECKOUT_SESSION_ID}',
-    cancel_url: 'https://main.dsxr40yvbyhag.amplifyapp.com/signedin',
-    client_reference_id: username,  // Set the client_reference_id to the username
-    metadata: {  // Add metadata field
-      username: username,  // Include username in the metadata
-    },
-  });
-  res.json({ id: session.id });
+    const response = await fetch(signedRequest.url, {
+        method: 'POST',
+        headers: signedRequest.headers,
+        body: JSON.stringify(requestBody)
+    });
+
+    const responseData = await response.json();
+    console.log("GraphQL response:", responseData);
+    return responseData;
+};
+
+app.post('/create-checkout-session', bodyParser.json(), async (req, res) => {  
+    console.log("Handling /create-checkout-session endpoint");
+
+    console.log("Raw request body:", req.body.toString());
+
+
+    const { username } = req.body;
+
+    const session = await stripe.checkout.sessions.create({
+        payment_method_types: ['card'],
+        line_items: [{
+            price: 'price_1Nb7sTGGfDL03j8nedwYY4zH',
+            quantity: 1,
+        }],
+        mode: 'subscription',
+        success_url: 'http://localhost:3000/success?session_id={CHECKOUT_SESSION_ID}',
+        cancel_url: 'http://localhost:3000/signedin',
+        client_reference_id: username,
+        metadata: {
+            username: username,
+        },
+    });
+
+    console.log("Stripe session created:", session);
+
+    const subscriptionData = {
+        stripeSubscriptionId: session.id,
+        plan: 'LSTM Model',
+        status: 'active',
+        userId: username
+    };
+
+    try {
+        const result = await makeGraphQLRequest(`
+            mutation CreateStripeSubscription($input: CreateStripeSubscriptionInput!) {
+                createStripeSubscription(input: $input) {
+                    id
+                    stripeSubscriptionId
+                }
+            }
+        `, { input: subscriptionData });
+        console.log("StripeSubscription record created:", result);
+    } catch (error) {
+        console.error("Error creating StripeSubscription record:", error);
+    }
+
+    res.json({ id: session.id });
 });
-
 
 app.post('/webhook', bodyParser.raw({type: 'application/json'}), async (req, res) => {
-  const sig = req.headers['stripe-signature'];
+    console.log("Handling /webhook endpoint");
 
-  let event;
+    console.log("Raw request body:", req.body.toString('utf8')); // Log the raw request body
 
-  try {
-    event = stripe.webhooks.constructEvent(req.body, sig, process.env.STRIPE_WEBHOOK_SECRET);
-  } catch (err) {
-    return res.status(400).send(`Webhook Error: ${err.message}`);
-  }
+    const sig = req.headers['stripe-signature'];
 
-    // Log the event type and data
+    let event;
+    try {
+        event = stripe.webhooks.constructEvent(req.body, sig, process.env.STRIPE_WEBHOOK_SECRET);
+    } catch (err) {
+        console.error(`Failed to verify webhook signature: ${err.message}`);
+        return res.status(400).send(`Webhook Error: ${err.message}`);
+    }
+
     console.log(`Received event with type ${event.type}`);
     console.log(`Event data: ${JSON.stringify(event.data)}`);
-  
 
-  // Handle the checkout.session.completed event
-  if (event.type === 'checkout.session.completed') {
-    console.log("checkout.session.completed event received");
-      const session = event.data.object;
+    if (event.type === 'checkout.session.completed') {
+        const session = event.data.object;
+        const username = session.client_reference_id;
 
-    // Get the user's username from the client reference ID
-    const username = session.client_reference_id;
+        console.log(`Username: ${username}`);
+        console.log(`Customer ID: ${session.customer}`);
 
-    console.log(`Username: ${username}`);
-    console.log(`Customer ID: ${session.customer}`);
+        try {
+            await cognito.adminUpdateUserAttributes({
+                UserPoolId: 'eu-west-2_d852nM2qL',
+                Username: username,
+                UserAttributes: [
+                    {
+                        Name: 'custom:stripeCustomerId',
+                        Value: session.customer,
+                    },
+                ],
+            }).promise();
+            console.log(`Updated Stripe customer ID for user ${username}`);
+        } catch (error) {
+            console.error(`Error updating user attributes: ${error}`);
+        }
 
-    // Update the user's Stripe customer ID in Cognito
-    try {
-      await cognito.adminUpdateUserAttributes({
-        UserPoolId: 'eu-west-2_d852nM2qL',
-        Username: username,
-        UserAttributes: [
-          {
-            Name: 'custom:stripeCustomerId',
-            Value: session.customer,
-          },
-        ],
-      }).promise();
-      console.log(`Updated Stripe customer ID for user ${username}`);
-    } catch (error) {
-      console.error(`Error updating user attributes: ${error}`);
+        const subscriptionData = {
+            stripeSubscriptionId: session.id,
+            plan: 'LSTM Model',
+            status: 'active',
+            userId: username
+        };
+
+        try {
+            const result = await makeGraphQLRequest(`
+                mutation UpdateStripeSubscription($input: UpdateStripeSubscriptionInput!) {
+                    updateStripeSubscription(input: $input) {
+                        id
+                        stripeSubscriptionId
+                    }
+                }
+            `, { input: subscriptionData });
+            console.log("StripeSubscription record updated:", result);
+        } catch (error) {
+            console.error("Error updating StripeSubscription record:", error);
+        }
     }
-  }
-    
 
-  // Handle the customer.subscription.deleted event
-  if (event.type === 'customer.subscription.deleted') {
-    const subscription = event.data.object;
+    if (event.type === 'customer.subscription.deleted') {
+        const subscription = event.data.object;
 
-    // Get the user's username from the client reference ID
-    const username = subscription.metadata.username;
+        const subscriptionData = {
+            id: subscription.id,
+            status: 'cancelled'
+        };
 
-    // Update the user's Stripe customer ID in Cognito to be empty
-    await cognito.adminUpdateUserAttributes({
-      UserPoolId: 'eu-west-2_d852nM2qL',
-      Username: username,
-      UserAttributes: [
-        {
-          Name: 'custom:stripeCustomerId',
-          Value: '',
-        },
-      ],
-    }).promise();
+        try {
+            const result = await makeGraphQLRequest(`
+                mutation UpdateStripeSubscription($input: UpdateStripeSubscriptionInput!) {
+                    updateStripeSubscription(input: $input) {
+                        id
+                        status
+                    }
+                }
+            `, { input: subscriptionData });
+            console.log("StripeSubscription record status updated to cancelled:", result);
+        } catch (error) {
+            console.error("Error updating StripeSubscription record status:", error);
+        }
+    }
 
-    console.log(`Cleared Stripe customer ID for user ${username}`);
-  }
-
-  res.json({received: true});
+    res.json({received: true});
 });
 
-
-/**********************
- * Example get method *
- **********************/
-
-app.get('/item', function(req, res) {
-  // Add your code here
-  res.json({success: 'get call succeed!', url: req.url});
-});
-
-app.get('/item/*', function(req, res) {
-  // Add your code here
-  res.json({success: 'get call succeed!', url: req.url});
-});
-
-/****************************
-* Example post method *
-****************************/
-
-app.post('/item', function(req, res) {
-  // Add your code here
-  res.json({success: 'post call succeed!', url: req.url, body: req.body})
-});
-
-app.post('/item/*', function(req, res) {
-  // Add your code here
-  res.json({success: 'post call succeed!', url: req.url, body: req.body})
-});
-
-/****************************
-* Example put method *
-****************************/
-
-app.put('/item', function(req, res) {
-  // Add your code here
-  res.json({success: 'put call succeed!', url: req.url, body: req.body})
-});
-
-app.put('/item/*', function(req, res) {
-  // Add your code here
-  res.json({success: 'put call succeed!', url: req.url, body: req.body})
-});
-
-/****************************
-* Example delete method *
-****************************/
-
-app.delete('/item', function(req, res) {
-  // Add your code here
-  res.json({success: 'delete call succeed!', url: req.url});
-});
-
-app.delete('/item/*', function(req, res) {
-  // Add your code here
-  res.json({success: 'delete call succeed!', url: req.url});
-});
-
-app.listen(3000, function() {
-    console.log("App started")
-});
-
-// Export the app object. When executing the application local this does nothing. However,
-// to port it to AWS Lambda we will create a wrapper around that will load the app from
-// this file
-module.exports = app
+module.exports = app;
