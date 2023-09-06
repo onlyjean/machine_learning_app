bfa772105126654bd9f5454d575e659d0240718c
Jean Isou <onlyjean8888@gmail.com>
2023-09-06 19:58:29 +0100

    stripeWebHookHandler and aws bacjend configured.

--- amplify/backend/function/stripeWebhookHandler/src/index.js
+++ amplify/backend/function/stripeWebhookHandler/src/index.js
@@ -2,6 +2,7 @@ const fetch = require('node-fetch');
 const aws4 = require('aws4');
 const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
 
+// AWS AppSync configuration
 const awsmobile = {
     graphqlEndpoint: "https://y4phlwteercpflrdx6pawip3dm.appsync-api.eu-west-2.amazonaws.com/graphql",
     region: "eu-west-2",
@@ -9,6 +10,7 @@ const awsmobile = {
     apiKey: "da2-q5rjn4gqwnfyza3h4pccmvcipm",
 };
 
+// GraphQL query for creating a subscription
 const createSubscriptionQuery = `
   mutation CreateStripeSubscription($input: CreateStripeSubscriptionInput!) {
     createStripeSubscription(input: $input) {
@@ -21,6 +23,7 @@ const createSubscriptionQuery = `
   }
 `;
 
+// GraphQL query for updating a subscription
 const updateSubscriptionQuery = `
   mutation UpdateStripeSubscription($input: UpdateStripeSubscriptionInput!) {
     updateStripeSubscription(input: $input) {
@@ -30,12 +33,14 @@ const updateSubscriptionQuery = `
   }
 `;
 
+// Function to make a GraphQL request
 const makeGraphQLRequest = async (query, variables) => {
     const requestBody = {
         query: query,
         variables: variables
     };
 
+    // Sign the request using AWS4
     const signedRequest = aws4.sign({
         method: 'POST',
         url: awsmobile.graphqlEndpoint,
@@ -48,12 +53,14 @@ const makeGraphQLRequest = async (query, variables) => {
         region: awsmobile.region
     });
 
+    // Make the fetch request
     const response = await fetch(signedRequest.url, {
         method: 'POST',
         headers: signedRequest.headers,
         body: JSON.stringify(requestBody)
     });
 
+    // Parse the response
     const responseData = await response.json();
     if (responseData.errors) {
         console.error("GraphQL errors:", responseData.errors);
@@ -61,10 +68,12 @@ const makeGraphQLRequest = async (query, variables) => {
     return responseData;
 };
 
+// Lambda function handler
 exports.handler = async (event) => {
     const sig = event.headers['stripe-signature'];
     let stripeEvent;
 
+    
     try {
         stripeEvent = stripe.webhooks.constructEvent(event.body, sig, process.env.STRIPE_WEBHOOK_SECRET);
     } catch (err) {
@@ -74,7 +83,7 @@ exports.handler = async (event) => {
             body: `Webhook Error: ${err.message}`
         };
     }
-
+    // Create a subscription record
     if (stripeEvent.type === 'checkout.session.completed') {
         const session = stripeEvent.data.object;
         const username = session.client_reference_id;
@@ -94,6 +103,7 @@ exports.handler = async (event) => {
         }
     }
 
+    // Handle customer.subscription.deleted event
     if (stripeEvent.type === 'customer.subscription.deleted') {
         const subscription = stripeEvent.data.object;
 
@@ -102,6 +112,7 @@ exports.handler = async (event) => {
             status: 'cancelled'
         };
 
+        // Update the subscription record to cancelled
         try {
             const result = await makeGraphQLRequest(updateSubscriptionQuery, { input: subscriptionData });
             console.log("StripeSubscription record status updated to cancelled:", result);
@@ -110,6 +121,7 @@ exports.handler = async (event) => {
         }
     }
 
+    // Return a 200 status code
     return {
         statusCode: 200,
         body: JSON.stringify({ received: true }),
